2648c3f468faa02829671b913e4e8770906dfbdb
liaojiahui <2392514318@qq.com>
2019-09-19 17:10:12 +0800

    解决了通知Mapper的冲突

--- src/main/java/com/hr/mapper/IAoaNoticeListMapper.java
+++ src/main/java/com/hr/mapper/IAoaNoticeListMapper.java
@@ -56,34 +56,5 @@ public interface IAoaNoticeListMapper {
      */
     public AoaNoticeList queryAoaNoticeListById(Long noticeId);
 
-    /**
-     * 根据用户ID 查询其通知内容
-     *
-     * @param paramMap 参数列表
-     * @return
-     */
-    public List<AoaNoticeList> queryAoaNoticeList(Map<String, Object> paramMap);
 
-    /**
-     * 修改通知置顶状态
-     *
-     * @return
-     */
-    public Integer updateAoaNoticeForTop(@Param("noticeId") Long noticeId, @Param("isTop") Integer isTop);
-
-    /**
-     * 获取总条目数
-     *
-     * @param paramMap
-     * @return
-     */
-    public Long getNoticeCount(Map<String, Object> paramMap);
-
-    /**
-     * 根据ID 查询指定通知
-     *
-     * @param noticeId 通知ID
-     * @return
-     */
-    public AoaNoticeList queryAoaNoticeListById(Long noticeId);
 }
